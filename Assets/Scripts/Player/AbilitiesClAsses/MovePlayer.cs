ca80395f3390a8c7efc60bbcff9d6176b8b836e2
Agsayt <agsayt@gmail.com>
2023-02-04 19:38:39 +0300

    Small refactory for MovePlayer

--- Assets/Scripts/Player/AbilitiesClAsses/MovePlayer.cs
+++ Assets/Scripts/Player/AbilitiesClAsses/MovePlayer.cs
@@ -9,7 +9,8 @@ public class MovePlayer : AbilityBase
 {
 
     public DirectionState direction = DirectionState.right;
-    public float movementSpeed = 0.01f;
+    public float movementSpeed = 0.005f;
+    private float speedNormalizer = 1000f;
 
     public enum DirectionState
     {
@@ -26,27 +27,25 @@ public class MovePlayer : AbilityBase
     }  
     
     private void directionChange(GameObject gameObject)
-    {           
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = direction is DirectionState.down || direction is DirectionState.up? 0 : 1;
+
         switch (direction)
         {
             case DirectionState.up:
-                gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + movementSpeed, 0);      
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + movementSpeed / speedNormalizer, 0);      
                 break;
 
             case DirectionState.down:
-                gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - movementSpeed, 0);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - movementSpeed / speedNormalizer, 0);
                 break;
 
             case DirectionState.right:
-                gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x + movementSpeed, gameObject.transform.position.y, 0);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + movementSpeed / speedNormalizer, gameObject.transform.position.y, 0);
                 break;
 
             case DirectionState.left:
-                gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x - movementSpeed, gameObject.transform.position.y, 0);
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - movementSpeed / speedNormalizer, gameObject.transform.position.y, 0);
                 break;
         }
     }
